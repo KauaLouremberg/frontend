ce257463bbcf02e987e5291ce443b0cdc317a5e6
Louremberg <kaua.lima@layoutsistemas.com.br>
2025-04-10 22:45:57 -0300

    Criação sidebar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
+    
     <Router>
       <Routes>
       <Route path="/" element={<Login />} />
---
+++ src/ElementosForm/Sidebar.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import {
+    DashboardOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+      token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <br></br>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <DashboardOutlined />,
+              label: 'Inicio',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'teste2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'teste3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout >
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+    )
+}
+
+export default Sidebar
